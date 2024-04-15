4bfdc3bde02144d2975d4a3126528c87b9366e09
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-04-15 18:37:09 +0200

    Add check for datetime to only send notifications about tweets from last 2 minutes

--- twitter_bot.py
+++ twitter_bot.py
@@ -5,22 +5,22 @@ import re
 import time
 import discord_notification
 import os
+import datetime
 
 id_list = []
 id_dict = {}
-cookies_value = "d_prefs=MjoxLGNvbnNlbnRfdmVyc2lvbjoyLHRleHRfdmVyc2lvbjoxMDAw; lang=en; gt=1778505775091962322; guest_id=v1%3A171286402257360862; _twitter_sess=BAh7CSIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCA7Tps6OAToMY3NyZl9p%250AZCIlMDVhYTIyMGNlOTE4NzQ0MmJhNzczMzJhNWRjN2Q5MDk6B2lkIiVlNmIy%250AMjY2N2I2NGE0ZDMwODNiNzMyNDk1ZjY4YTk1Yw%253D%253D--f181716dd4dc885166c379892df9fd21724196fa; kdt=S2RG1OTATl9kHpJGi3bNfYWbYJiahmspdeKSK2iI; auth_token=59f0a511212ac5205468aa267f4f0583c60ff337; ct0=520d4edb8c8bcc629504bce13b759361d08e07b2063eff9ce8a75abb3f52f0bf781693d791c4c569939e9e4023669b72f3929ba34c5cbede844b0c2de47b214a40480504345bdd52c696659d468f74e1; twid=u%3D1171793020259373058"
+cookies_value = "d_prefs=MjoxLGNvbnNlbnRfdmVyc2lvbjoyLHRleHRfdmVyc2lvbjoxMDAw; kdt=S2RG1OTATl9kHpJGi3bNfYWbYJiahmspdeKSK2iI; lang=en; dnt=1; guest_id=v1%3A171319706116288525; gt=1779903600241651920; _twitter_sess=BAh7CSIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCNGhgOKOAToMY3NyZl9p%250AZCIlMzM3MDhlZDIzYzA2NDFkZTViOWI5NDQ5ZDMyNjkyYzM6B2lkIiU0NWE5%250ANTUwNTQ1ZGVjOGFiNDkxMjVhNmUzNTE5YTBhZg%253D%253D--d5e38cccbf4e9eca8df616d29d7961da1f706953; auth_token=14249c18a260d202da0b3e2f3824d5840b895e65; ct0=289ad9a9cbcbe142c076f32d00fcd21d3841787e9485d6ba32b7168190691b657962d322b49d46d2d33c5f82a6efa3172b244b4753944fb87a598396c3effdc1660c5231dbb2fc2396f9b2178f25dd37; twid=u%3D2403922286; att=1-zHg87l7326miiqMmuuqGC876ScfOvaT7zuPWehpj"
 
 # Log into Twitter
 app = Twitter("session")
 app.load_cookies(cookies_value)
 print(app.me)
 
+# Functions
 def reset_database():
     database_file = 'ids_database.db'
     if os.path.exists(database_file):
         os.remove(database_file)
-
-
 def create_id_table():
     conn = sqlite3.connect('ids_database.db')
     cursor = conn.cursor()
@@ -33,7 +33,6 @@ def create_id_table():
     ''')
     conn.commit()
     conn.close()
-
 def check_if_id_exists(id):
     # Connect to SQLite database
     conn = sqlite3.connect('ids_database.db')
@@ -51,7 +50,6 @@ def check_if_id_exists(id):
 
     # Close connection
     conn.close()
-
 def insert_id_into_db(id):
     conn = sqlite3.connect('ids_database.db')
     cursor = conn.cursor()
@@ -64,35 +62,59 @@ def insert_id_into_db(id):
     # Commit changes and close connection
     conn.commit()
     conn.close()
-
 def fetch_home_timeline():
     tweets = app.get_home_timeline(timeline_type=HOME_TIMELINE_TYPE_FOLLOWING)
     for tweet in tweets:  
         id_list.append(int(tweet.id))
         id_dict[tweet.id] = {}
         id_dict[tweet.id]['url'] = tweet.url
-        id_dict[tweet.id]['date'] = tweet.date
+        id_dict[tweet.id]['date'] = tweet.date.replace(tzinfo=None)
         id_dict[tweet.id]['text'] = tweet.text
         id_dict[tweet.id]['author'] = tweet.author
         id_dict[tweet.id]['is_quoted'] = tweet.is_quoted
     #     print(tweet.id, tweet.date, tweet.text, tweet.author, tweet.is_quoted, tweet.is_retweeted, tweet.url, tweet.source)
     # print(id_list)
 
+# Main Code
 reset_database()
 create_id_table()
 fetch_home_timeline()
 
-print(id_dict)
-for key, value in id_dict.items():
-    print(key, value)
+# test_time = 0
+
+# print(id_dict)
+# for key, value in id_dict.items():
+#     print(key, value)
+#     print(value['date'])
+#     test_time = value['date']
+
+# # test_time = test_time.replace(tzinfo=None)
+# time_now = datetime.datetime.utcnow()
+# print('===================')
+# print(time_now)
+# print(test_time)
+
+
+# print(type(time_now))
+# print(type(test_time))
+
+
+# time_difference = time_now - test_time
+# print(time_difference)
+# print(type(time_difference))
+# print(time_difference.total_seconds())
 
 # Add a "if date is within last 2 minutes combined with if id not in databse -> discord notification"
 # thats gonna be the solution to making it ignore older tweets and not spam at the very beginning too much with older stuff
 
-# while True:
-#     fetch_home_timeline()
-#     for key, value in id_dict.items():
-#         id_exist = check_if_id_exists(key)
-#         if id_exist:
-#             discord_notification.send_discord_notification(value)
-#     time.sleep(15)
+while True:
+    current_time = datetime.datetime.utcnow()
+    fetch_home_timeline()
+    for key, value in id_dict.items():
+        id_exist = check_if_id_exists(key)
+        if id_exist:
+            time_difference = current_time - value['date']
+            if time_difference.total_seconds() <= 120:
+                discord_notification.send_discord_notification(value['url'])
+                time.sleep(1)
+    time.sleep(15)
